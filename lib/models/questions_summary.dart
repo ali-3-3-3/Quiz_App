15cd7467c4846f89e9324c087d057e18047c0378
Ali <105072289+aliya-333@users.noreply.github.com>
2023-07-12 00:59:22 +0800

    Finished Quiz App!

--- lib/models/questions_summary.dart
+++ lib/models/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -6,35 +7,95 @@ class QuestionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
 
   @override
-  Widget build(context) {
-    return Column(
-      children: summaryData.map(
-        (data) {
-          return Row(
-            children: [
-              Text(
-                ((data['question_index'] as int) + 1).toString(),
-              ),
-              Column(
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              bool correct = data['user_answer'] == data['correct_answer'];
+              return Row(
                 children: [
-                  Text(
-                    data['question'] as String,
+                  ClipOval(
+                    child: Material(
+                      color: correct
+                          ? const Color.fromARGB(255, 147, 189, 156)
+                          : const Color.fromARGB(255, 213, 93, 84),
+                      child: InkWell(
+                        child: SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: Text(
+                            ((data['question_index'] as int) + 1).toString(),
+                            style: GoogleFonts.lato(
+                              color: const Color.fromARGB(255, 40, 2, 49),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                   const SizedBox(
-                    height: 5,
+                    width: 20,
                   ),
-                  Text(
-                    data['user_answer'] as String,
-                  ),
-                  Text(
-                    data['correct_answer'] as String,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            data['question'] as String,
+                            style: GoogleFonts.lato(
+                              color: Colors.amber,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            data['user_answer'] as String,
+                            style: GoogleFonts.lato(
+                              color: const Color.fromARGB(194, 243, 143, 226),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            data['correct_answer'] as String,
+                            style: GoogleFonts.lato(
+                              color: const Color.fromARGB(194, 143, 243, 173),
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    ),
                   ),
                 ],
-              ),
-            ],
-          );
-        },
-      ).toList(),
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
